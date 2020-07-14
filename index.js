ca588fd2b2e6be53280521513a5b6e6c9bc192df
Megan Comeau <mlcomeau99@gmail.com>
2020-07-14 22:23:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// Your code goes here
-
 document.addEventListener("DOMContentLoaded", function() {
-  let text = document.getElementByID('text');
-  text.textContent = "This is really cool!";
+  updateDOM()
 });
+
+function updateDOM() {
+  document.getElementById("text")
+  .innerHTML = "This is really cool!";
+}
