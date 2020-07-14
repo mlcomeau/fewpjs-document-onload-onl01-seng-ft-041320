49627e032faa79c1678632a517aaf1ae6877b1b5
Megan Comeau <mlcomeau99@gmail.com>
2020-07-14 22:20:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
+  document.getElementByID('text').textContent = "This is really cool!";
 });
